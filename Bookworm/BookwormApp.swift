c2e68ee39d6eda24c287967c28fa37aa7f4cfd4f
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-03-18 22:16:00 -0600

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Mitch Andrade on 3/18/22.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
