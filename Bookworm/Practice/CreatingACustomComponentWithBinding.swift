54a48baf4835fefaf7cd939cd3c2600be8ba5fe2
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-03-19 11:01:22 -0600

    first commit

---
+++ Bookworm/Practice/CreatingACustomComponentWithBinding.swift
@@ -0,0 +1,20 @@
+//
+//  CreatingACustomComponentWithBinding.swift
+//  Bookworm
+//
+//  Created by Mitch Andrade on 3/19/22.
+//
+
+import SwiftUI
+
+struct CreatingACustomComponentWithBinding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreatingACustomComponentWithBinding_Previews: PreviewProvider {
+    static var previews: some View {
+        CreatingACustomComponentWithBinding()
+    }
+}
